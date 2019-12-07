348f6d2a1a58c417a149f1e0eb2ae178948e9a5b
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-12-07 16:18:56 +0300

    Update ex3.py

--- lesson 1/ex3.py
+++ lesson 1/ex3.py
@@ -1,11 +1,11 @@
-i = 0
-while i != 101:
+a = list(range(1, 101, 1))
+for i in a:
     i+=1
-    if i%3:
-        print('Fizz')
-    elif i%5:
-        print('Buzz')
-    elif i%15:
+    if i%15==0:
         print('FizzBuzz')
+    elif i%5==0:
+        print('Buzz')
+    elif i%3==0:
+        print('Fizz')
     else:
         print(i)
