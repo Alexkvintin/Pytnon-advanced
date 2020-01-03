0393fad71d3632cc0a466a34ba7672d847b2f6b0
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-01-04 02:09:47 +0300

    Update ex2.py

--- lesson4/ex2.py
+++ lesson4/ex2.py
@@ -1,65 +1,86 @@
 import datetime
 
-d = [{'login': 'admin', 'password': 'admin', 'users': None}, ]
+d = [{'login' : 'admin', 'password' : 'admin'}, ]
 
-class Reg:
-    
-    def __init__(self, login, password):
+
+class Reg :
+
+    def __init__(self, login, password) :
         self._login = login
         self._password = password
-    def add_(x):
-        x.append({'login': self._login, 'password': self._password})
+
+    def _add(self) :
+        d.append({'login' : str(login), 'password' : str(password),
+                  'date' : (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year),
+                  'posts' : None})
+        return d
 
 
-class Autor(Reg):
-    def __init__(self, login, password):
+class Admin(Reg) :
+    def admin(self) :
+        print('Добро пожаловать', login)
+        print(d)
+
+
+class Autor(Reg) :
+    def __init__(self, login, password) :
         super().__init__(login, password)
+        if self._login == 'admin' and self._password == 'admin' :
+            Admin.admin(self)
+        else :
+            User.search(self)
 
 
-class Admin:
-    pass
+class User(Autor) :
+    def search(self) :
+        for i in d :
+            try :
+                if i['login'] == login and i['password'] == password :
+                    print('Добро пожаловать', login, '\n',
+                          i)
+                    break
+            except :
+                print('пользователь не верно введенные логин или пароль!')
+
+    def new_post(x) :
+        for i in d :
+            if i['login'] == login and i['password'] == password :
+                t = (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
+                if i['posts'] is None:
+                    b = [t, x]
+                    i['posts'] = b
+                    break
+                else:
+                    g = [t, x]
+                    i['posts'].append(g)
 
 
-class User:
-    pass
-
-
-user = []
-d = [{'login': 'admin', 'password': 'admin', 'users': user}, ]
-while True:
-
-    try:
+while True :
+    try :
         sing = int(input('хотите войти (1) или зарегистрироваться (0)'))
-    except:
+    except :
         sing = 0
 
-    if sing == 1:
-        login = input('Введите логин:')
-        password = input('Введите пароль:')
-        if login == 'admin' and password == 'admin' :
-            print('-'*80)
-            print('вы вошли как администротор')
-            print(d[1:][:])
-            break
-        elif login != 'admin' and password != 'admin' :
-            for i in d:
-                try:
-                    if i['login'] == login and i['password'] == password:
-                        print(i)
+    if sing == 1 :
+        login = input('введите логин')
+        password = input('введите пароль')
+        sin = Autor(login, password)
+        if login != 'admin' and password != 'admin' :
+            x = input('хотите написать пост? (1-да enter-нет)')
+            if x == '1' :
+                while True :
+                    _time = (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
+                    data = input('напишите пост')
+                    user = User.new_post(data)
+                    y = input('хотите написать еще пост ? (1-да enter-нет)')
+                    if y == '1' :
+                        continue
+                    else :
                         break
-                except:
-                    print('error')
-        else:
-            f = input('неверный логин или пароль, хотите попробывать еще раз ?')
-            if f == 1:
-                break
-    elif sing == 0:
-        login = input('Введите логин для регистрации:')
-        password = input('Введите пароль для регистрации:')
-        new = {'login': login,
-               'password': password,
-               'date': (datetime.datetime.now().hour, datetime.datetime.now().minute, datetime.datetime.now().year)}
-        d.append(new)
-
-
-new_post = str(input())
+            else :
+                pass
+    elif sing == 0 :
+        login = input('input login')
+        password = input('input password')
+        New = Reg(login, password)
+        New._add()
