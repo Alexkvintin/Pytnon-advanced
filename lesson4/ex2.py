fc442847d65fcf121d387320a99b6a6948bae61e
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-01-05 18:56:54 +0200

    Update ex2.py

--- lesson4/ex2.py
+++ lesson4/ex2.py
@@ -1,50 +1,67 @@
 import datetime
+d = [{'login': 'admin', 'password': 'admin'}, ]
 
-d = [{'login' : 'admin', 'password' : 'admin'}, ]
 
+class Reg:
 
-class Reg :
-
-    def __init__(self, login, password) :
+    def __init__(self, login, password):
         self._login = login
         self._password = password
 
-    def _add(self) :
-        d.append({'login' : str(login), 'password' : str(password),
-                  'date' : (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year),
-                  'posts' : None})
-        return d
+    def _add(self):
+        f = 0
+        for i in d:
+            if i['login'] == self._login:
+                print('Такой пользователь уже зарегистрирован ! ')
+            else:
+                f += 1
+                pass
+            if len(self._password) >= 8 and any([x.isdigit() for x in self._password]) == True and any(
+                    [x.isupper() for x in self._password]) == True:
+                f += 1
+            else:
+                print('слишком простой пароль ! ')
+            if f == 2:
+                d.append({'login': str(login), 'password': str(password),
+                          'date': (
+                              datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year),
+                          'posts': None})
+                print('Регистрация успешна ! ')
+                return d
 
 
-class Admin(Reg) :
-    def admin(self) :
-        print('Добро пожаловать', login)
-        print(d)
+class Admin(Reg):
+    def admin(self):
+        print('-' * 35)
+        print('Добро пожаловать', login, '\n', d, '\n', '-' * 35)
 
 
-class Autor(Reg) :
-    def __init__(self, login, password) :
+class Autor(Reg):
+    def __init__(self, login, password):
         super().__init__(login, password)
-        if self._login == 'admin' and self._password == 'admin' :
+        if self._login == 'admin' and self._password == 'admin':
             Admin.admin(self)
-        else :
+        else:
             User.search(self)
 
 
-class User(Autor) :
-    def search(self) :
-        for i in d :
-            try :
-                if i['login'] == login and i['password'] == password :
-                    print('Добро пожаловать', login, '\n',
-                          i)
-                    break
-            except :
-                print('пользователь не верно введенные логин или пароль!')
+class User(Autor):
+    u = 0
+    def search(self):
+        u = 0
+        for i in d:
+            if i['login'] == login and i['password'] == password:
+                print('-' * 35)
+                print('Добро пожаловать', login, '\n', i, '\n', '-' * 35)
+                u += 1
+                break
+            elif u != 1:
+                print('не верно введенные логин или пароль!')
+        return u
 
-    def new_post(x) :
-        for i in d :
-            if i['login'] == login and i['password'] == password :
+    def new_post(x):
+        for i in d:
+            if i['login'] == login and i['password'] == password:
                 t = (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
                 if i['posts'] is None:
                     b = [t, x]
@@ -55,31 +72,31 @@ class User(Autor) :
                     i['posts'].append(g)
 
 
-while True :
-    try :
+while True:
+    try:
         sing = int(input('хотите войти (1) или зарегистрироваться (0)'))
-    except :
+    except:
         sing = 0
 
-    if sing == 1 :
+    if sing == 1:
         login = input('введите логин')
         password = input('введите пароль')
         sin = Autor(login, password)
-        if login != 'admin' and password != 'admin' :
+        if login != 'admin' and password != 'admin':
             x = input('хотите написать пост? (1-да enter-нет)')
-            if x == '1' :
-                while True :
+            if x == '1':
+                while True:
                     _time = (datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year)
                     data = input('напишите пост')
                     user = User.new_post(data)
                     y = input('хотите написать еще пост ? (1-да enter-нет)')
-                    if y == '1' :
+                    if y == '1':
                         continue
-                    else :
+                    else:
                         break
-            else :
+            else:
                 pass
-    elif sing == 0 :
+    elif sing == 0:
         login = input('input login')
         password = input('input password')
         New = Reg(login, password)
