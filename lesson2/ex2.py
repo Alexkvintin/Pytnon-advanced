f6e4d55d00913a897fd9bf96f1681570f4814dcf
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-01-09 15:41:31 +0300

    Update ex2.py

--- lesson2/ex2.py
+++ lesson2/ex2.py
@@ -1,48 +1,48 @@
-class Mag:
-    def __init__(self, name, quantity_sold):
+class Mag :
+    _total_sales = 0
+
+    def __init__(self, name, quantity_sold) :
         self._name = name
-        try:
+        try :
             self._quantity_sold = int(quantity_sold)
-        except ValueError:
+            Mag._total_sales += self._quantity_sold
+        except ValueError :
             print('Количество должно быть целым числом !')
             self._quantity_sold = 0
 
-    def sold(self, new_sold):
-         try:
-             self._quantity_sold = self._quantity_sold + new_sold
-         except TypeError:
-             print('Значение должно быть целым числом !')
+    def sold(self, new_sold) :
+        try :
+            self._quantity_sold = self._quantity_sold + new_sold
+            Mag._total_sales += new_sold
+        except TypeError :
+            print('Значение должно быть целым числом !')
 
-
-    def mag_info(self):
+    def mag_info(self) :
         return dict(
             name=self._name,
             quantity_sold=self._quantity_sold)
 
-    def sum_(*x):
-       a = 0
-       for i in x:
-           a+=i
-       return f'общее количество продаж : {a}'
+    def total_sales(self):
+        return f'общее количество продаж : {Mag._total_sales}'
 
 
 mag1 = Mag('allo', '4a')
-print('-'*80)
+print('-' * 80)
 print(mag1.mag_info())
-print('-'*80)
+print('-' * 80)
 mag2 = Mag('ttt', 12)
 print(mag2.mag_info())
-print('-'*80)
+print('-' * 80)
 mag3 = Mag('rozetka', 30)
 print(mag3.mag_info())
-print('-'*80)
+print('-' * 80)
 mag1.sold(2)
 print(mag1.mag_info())
-print('-'*80)
+print('-' * 80)
 mag2.sold('r')
 print(mag2.mag_info())
-print('-'*80)
+print('-' * 80)
 mag3.sold(0)
 print(mag3.mag_info())
-print('-'*80)
-print(Mag.sum_(mag1._quantity_sold, mag2._quantity_sold, mag3._quantity_sold))
+print('-' * 80)
+print(Mag.total_sales(True))
