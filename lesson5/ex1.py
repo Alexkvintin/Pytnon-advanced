7c7e31e0dcfd5037b65b967a32ce6ad91b7545f7
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-01-17 16:29:43 +0300

    Update ex1.py

--- lesson5/ex1.py
+++ lesson5/ex1.py
@@ -1,15 +1,21 @@
 import threading
 
 
-def decor() :
+def decor(isdeamon, name) :
     def wrapper(func, ) :
-        tr = threading.Thread(target=func, args=(1,))
-        return tr.start()
+        tr = threading.Thread(target=func, args=(name,))
+        tr.daemon = isdeamon
+        tr.setName(name)
+        tr.start()
+        tr.join()
+        return
 
     return wrapper
 
 
-@decor()
+@decor(True, 'rtx')
 def now_ran(nametr) :
     print(f"Thread {nametr}: starting")
     print(f"Thread {nametr}: finishing")
+
+
