9ef51441f93e57d790becbef12ba4f718f4d53e7
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-01-17 16:24:08 +0300

    Update ex1.py

--- lesson5/ex1.py
+++ lesson5/ex1.py
@@ -1,1 +1,15 @@
+import threading
 
+
+def decor() :
+    def wrapper(func, ) :
+        tr = threading.Thread(target=func, args=(1,))
+        return tr.start()
+
+    return wrapper
+
+
+@decor()
+def now_ran(nametr) :
+    print(f"Thread {nametr}: starting")
+    print(f"Thread {nametr}: finishing")
