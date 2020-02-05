629f8ca50e23bc6acce8a21439252001ab14f15d
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-02-05 21:52:23 +0200

    Update ex2.py

--- lesson5/ex2.py
+++ lesson5/ex2.py
@@ -1,1 +1,42 @@
-1
+import threading
+import wget
+import os
+
+urls = ('https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        'https://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/1024/rick-icon.png',
+        )
+
+
+def decorator(isdaemon, name):
+    def take_func(func):
+        def wrapper():
+            for i, url in enumerate(urls, start=1):
+                tr = threading.Thread(target=func, name=name, daemon=isdaemon, args=(name, i, url))
+                threads.append(tr)
+                tr.start()
+            for i in threads:
+                i.join()
+
+        return wrapper
+
+    return take_func
+
+
+@decorator(True, 'name')
+def new_run(name, i, url):
+    print(f'Thread {name} {i}: Начал загрузку !')
+    wget.download(url, f'Картинка {i}.png')
+    print(f'Thread {name} {i}: Завершил загрузку !')
+
+
+threads = []
+
+new_run()
