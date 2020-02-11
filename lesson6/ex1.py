3acabbbc5c5f15dc2ed1c4cc151fb9f019d9ba3b
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2020-02-11 18:32:18 +0200

    Update ex1.py

--- lesson6/ex1.py
+++ lesson6/ex1.py
@@ -2,35 +2,36 @@ class List:
     def __init__(self, *args):
         self._list = [*args]
 
-    def pop(self, number):
-        return f'Был удален елемент {self._list.pop(number)}, по адрессу {number}'
-
     def append(self, item):
         self._list.append(item)
         return f'Был добавлен елемент {self._list[-1]}'
 
-    def insert(self, number, item):
-        self._list.insert(number, item)
-        return f'Был добавлен елемент {item}, по адрессу {number} '
-
-    def remove(self, item):
-        self._list.remove(item)
-        return f'Был удален елемент {item}'
+    def pop(self, number):
+        return f'Был удален елемент {self._list.pop(number)}, по адрессу {number}'
 
     def clear(self):
         self._list = []
         return 'Список очищен'
 
-    def len1(self):
-        return f'Количество символов {len(self._list)}'
+    def remove(self, item):
+        self._list.remove(item)
+        return f'Был удален елемент {item}'
+
+    def insert(self, number, item):
+        self._list.insert(number, item)
+        return f'Был добавлен елемент {item}, по адрессу {number} '
 
     def __str__(self):
         return str(self._list)
 
+    def __add__(self, other):
+        for values in other:
+            self.append(values)
+        return f'Были добавлены {other}'
+
 
 l = List(1, 2, 3, 4)
 print(l)
-print(l.len1())
 print(l.pop(2))
 print(l)
 print(l.append('5'))
@@ -40,4 +41,8 @@ print(l)
 print(l.remove('name'))
 print(l)
 print(l.clear())
+print(l)
+p = [1, 2, 4]
+l.append(4)
+print(l+p)
 print(l)
